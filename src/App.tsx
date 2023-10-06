53ba89f3108ecd85031d0481e30a694d44881445
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2023-10-06 18:23:06 -0500

    merging with main

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import { createContext, SetStateAction, useEffect, useState } from "react";
 import { ThemeProvider } from "@emotion/react";
@@ -7,71 +8,71 @@ import UserLocationPanel from "./Components/UserLocationPanel";
 import { getGeolocation } from "./Components/geolocator";
 import { DailyWeatherReports } from "./Components/weatherReports";
 import {
-    AreaChartDataType,
-    AreaChartHandlerProps,
-    DayWeatherData,
-    HourlyWeatherDataType,
-    WeatherDataType,
+	AreaChartDataType,
+	AreaChartHandlerProps,
+	DayWeatherData,
+	HourlyWeatherDataType,
+	WeatherDataType,
 } from "./lib/interfaces";
 import { Pending } from "@mui/icons-material";
 //const WeatherAPIsrc = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${API key}&units=${/* Metric or Imperial */}`;
 const WeatherAPISrc = `FarTestWeatherData.json`;
 type Coords = [number, number];
 interface User {
-    id?: string | undefined;
-    loc?: Coords;
-}
-interface Elem {
-    temp: { day: number };
+	id?: string | undefined;
+	loc?: Coords;
 }
 
 const LoadingScreen = () => {
-    return <Pending />;
+	return <Pending />;
 };
-function convertDataForReport(data: HourlyWeatherDataType[]) {}
 function App() {
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [hasLocation, setHasLocation] = useState(false);
-    const [userCoords, setUserCoords] = useState<Coords>();
-    const [userWeather, setUserWeather] = useState<WeatherDataType>();
-    const [weeklyUserWeather, setWeeklyUserWeather] =
-        useState<DayWeatherData[]>();
+	const [isLoaded, setIsLoaded] = useState(false);
+	const [hasLocation, setHasLocation] = useState(false);
+	const [userCoords, setUserCoords] = useState<Coords>();
+	const [userWeather, setUserWeather] = useState<WeatherDataType>();
+	const [weeklyUserWeather, setWeeklyUserWeather] = useState<DayWeatherData[]>();
 
-    //Async User Location Data Request
-    const resolveCallback = (result: any) => {
-        setUserCoords(result);
-        setHasLocation(true);
-    };
-    const failCallback = (error: any) => {
-        console.log("getGeolocation Failed");
-        console.log(error.message);
-    };
-    function asyncUserLocation() {
-        console.log("called async");
-        getGeolocation.then(resolveCallback, failCallback);
-    }
-    asyncUserLocation();
+	//Async User Location Data Request
+	const resolveCallback = (result: any) => {
+		setUserCoords(result);
+		setHasLocation(true);
+	};
+	const failCallback = (error: any) => {
+		console.log("getGeolocation Failed");
+		console.log(error.message);
+	};
+	function asyncUserLocation() {
+		console.log("called async");
+		getGeolocation.then(resolveCallback, failCallback);
+	}
+	asyncUserLocation();
 
-    const handleManualCoords = (loc: any) => {
-        setUserCoords(loc);
-        setHasLocation(true);
-        console.log("Setting Coords: " + loc);
-    };
-    const GetOpenWeatherData = async (coords: Coords) => {
-        fetch(WeatherAPISrc)
-            .then((res) => res.json())
-            .then(
-                (result) => {
-                    console.log(result);
-                    setUserWeather(result);
-                },
-                (error) => {
-                    console.log(error.message);
-                    console.log("Error with fetching weather data");
-                }
-            );
-    };
-    /*     useEffect(() => {
+	const handleManualCoords = (loc: any) => {
+		setUserCoords(loc);
+		setHasLocation(true);
+		console.log("Setting Coords: " + loc);
+	};
+	const LocationHandler = () => {
+		return (
+			<Box>{hasLocation ? <></> : <UserLocationPanel submitCoords={handleManualCoords} isActive={!hasLocation} />}</Box>
+		);
+	};
+	const GetOpenWeatherData = async (coords: Coords) => {
+		fetch(WeatherAPISrc)
+			.then((res) => res.json())
+			.then(
+				(result) => {
+					console.log(result);
+					setUserWeather(result);
+				},
+				(error) => {
+					console.log(error.message);
+					console.log("Error with fetching weather data");
+				}
+			);
+	};
+	/*     useEffect(() => {
         let ignore = false;
         console.log("Mounted");
 
@@ -81,13 +82,13 @@ function App() {
         };
     }, []);
  */
-    useEffect(() => {
-        if (userCoords) {
-            console.log("Getting weather data for coordinates: " + userCoords);
-            GetOpenWeatherData(userCoords);
-        }
-    }, [userCoords]);
-    /*  useEffect(() => {
+	useEffect(() => {
+		if (userCoords) {
+			console.log("Getting weather data for coordinates: " + userCoords);
+			GetOpenWeatherData(userCoords);
+		}
+	}, [userCoords]);
+	/*  useEffect(() => {
         if (userWeather?.hourly) {
             //convertDataForReport(userWeather.hourly);
         }
@@ -95,45 +96,29 @@ function App() {
             setWeeklyUserWeather(sanitizeDailyWeatherData(userWeather.daily));
         }
     }, [userWeather]); */
-    useEffect(() => {
-        console.log("Re-rendering");
-        /* let ignore = false;
+	useEffect(() => {
+		console.log("Re-rendering");
+		/* let ignore = false;
 		if (!ignore) {
 			userWeather ? DailyWeatherReports(userWeather.daily) : console.log("Error: unexpected weather data");
 		} */
-    }, [userWeather]);
-    return (
-        <ThemeProvider theme={siteTheme}>
-            <Container
-                id="App"
-                component="main"
-                disableGutters={true}
-                maxWidth={false}
-                sx={{ flexDirection: "column" }}
-            >
-                <CssBaseline enableColorScheme />
-                {hasLocation ? (
-                    <></>
-                ) : (
-                    <UserLocationPanel
-                        submitCoords={handleManualCoords}
-                        isActive={!hasLocation}
-                    />
-                )}
-                {userWeather?.daily ? (
-                    <DailyWeatherReports
-                        WeatherData={userWeather.daily}
-                        hourlyWeatherData={userWeather.hourly}
-                    />
-                ) : (
-                    <LoadingScreen />
-                )}
-                {/* <Box sx={{ flexGrow: "1", height: "50%", width: 1 / 1 }}>
+	}, [userWeather]);
+	return (
+		<ThemeProvider theme={siteTheme}>
+			<Container id="App" component="main" disableGutters={true} maxWidth={false} sx={{ flexDirection: "column" }}>
+				<CssBaseline enableColorScheme />
+				<LocationHandler />
+				{userWeather?.daily ? (
+					<DailyWeatherReports WeatherData={userWeather.daily} hourlyWeatherData={userWeather.hourly} />
+				) : (
+					<LoadingScreen />
+				)}
+				{/* <Box sx={{ flexGrow: "1", height: "50%", width: 1 / 1 }}>
 					{userWeather?.hourly ? <WeatherAreaChart weatherData={userWeather} /> : <LoadingScreen />}
 				</Box> */}
-            </Container>
-        </ThemeProvider>
-    );
+			</Container>
+		</ThemeProvider>
+	);
 }
 
 export default App;
--- src/Components/UserLocationPanel.tsx
+++ src/Components/UserLocationPanel.tsx
@@ -9,18 +9,21 @@ import {
 	Paper,
 	Select,
 	SelectChangeEvent,
+	TextField,
 	Typography,
 } from "@mui/material";
 import { Container } from "@mui/system";
-import { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, CSSProperties, useEffect, useState } from "react";
 import { getCoords, getGeocode, getGeolocation } from "./geolocator";
 //import Geolocator from "./geolocator";
 /* interface ChildProps {
     onEnterKey: (e: {    keyCode: number;}, loc: any) => void
     isActive: boolean;
 } */
+
 type Coords = [number, number] | undefined;
-type locType = "Zip Code" | "City" | "Coordinates";
+const LOCATION_TYPES = ["Zip Code", "City", "Coordinates"];
+type locType = (typeof LOCATION_TYPES)[number]; //"Zip Code" | "City" | "Coordinates";
 /* const UserLocationPanel = ({
 	onEnterKey,
 	value,
@@ -31,27 +34,82 @@ type locType = "Zip Code" | "City" | "Coordinates";
 	isActive: boolean;
     }) => { */
 
+const inputStyle: CSSProperties = {
+	boxSizing: `border-box`,
+	border: `1px solid transparent`,
+	width: `240px`,
+	height: `32px`,
+	padding: `0 12px`,
+	borderRadius: `3px`,
+	boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+	fontSize: `14px`,
+	outline: `none`,
+	textOverflow: `ellipses`,
+	position: "absolute",
+	top: "10px",
+	right: "10px",
+};
+
 const UserLocationPanel = ({ submitCoords, isActive }: { submitCoords: any; isActive: boolean }) => {
 	const [isOpen, setIsOpen] = useState(true);
-	const [addressType, setAddressType] = useState<string>("Zip Code");
+	const [addressType, setAddressType] = useState<locType>("Zip Code");
+	const [userAddress, setUserAddress] = useState<string>("");
 	const [zipCode, setZipCode] = useState<string>("75230");
 	const [city, setCity] = useState<string>("");
 	const [isInputError, setIsInputError] = useState<boolean>(false);
 	const [isValid, setIsValid] = useState<boolean>(false);
+	const [helperText, setHelperText] = useState<string>("");
 
 	const zipCodeRegEx = /(^\d{5}$)|(^\d{9}$)|(^\d{5}-\d{4}$)/;
-
 	const handleEnterKey = async (e: { keyCode: number }) => {
 		if (e.keyCode == 13) {
 			if (zipCodeRegEx.test(zipCode)) {
+				console.log("Enter key pressed");
 				setIsInputError(false);
 				getGeocode(zipCode).then((value) => submitCoords(value));
 				//setIsValid(true);
 			} else {
+				setHelperText("Please enter a valid 5 digit US Zip Code");
 				setIsInputError(true);
 			}
 		}
 	};
+
+	const handleChange = (e: any) => {
+		const regex = /^[0-9\b]+$/;
+		console.log(e.keycode);
+		if (e.target.value === "" || regex.test(e.target.value)) {
+			setZipCode(e.target.value);
+			setIsInputError(false);
+			setHelperText("");
+		} else {
+			setIsInputError(true);
+			setHelperText("Please enter numbers only");
+		}
+	};
+
+	const resolveGeoCallback = (result: any) => {
+		return submitCoords(result);
+	};
+	const failGeoCallback = (error: any) => {
+		console.log("getGeolocation Failed");
+		console.log(error.message);
+	};
+	/*
+TODO:
+     * useEffect(async () => {
+		const p = await getGeolocation.then(resolveGeoCallback, failGeoCallback);
+		const { promise, cancel } = cancellablePromise(p);
+		promise.then((d: any) => {
+			console.log("Promise resolved");
+			console.log(d);
+			submitCoords(d);
+		});
+		const clear = async () => {
+			await cancel();
+		};
+		clear();
+	}, []); */
 
 	return (
 		<Backdrop
@@ -66,7 +124,9 @@ const UserLocationPanel = ({ submitCoords, isActive }: { submitCoords: any; isAc
 					</Typography>
 					<Divider />
 					<Typography align="center">Please enable location services</Typography>
-					<Typography align="center">OR</Typography>
+					<br />
+					<Typography align="center">Or search by locattion</Typography>
+					<br />
 					<FormGroup row={true} sx={{ alignItems: "center", justifyContent: "center" }}>
 						<Typography align="center" sx={{ width: 1 / 3 }}>
 							Enter your
@@ -84,17 +144,20 @@ const UserLocationPanel = ({ submitCoords, isActive }: { submitCoords: any; isAc
 								{/* <MenuItem value={"Address"}>Address</MenuItem> */}
 							</Select>
 						</FormControl>
-					</FormGroup>
-					<FormControl>
-						<InputLabel>Zip Code</InputLabel>
-						<Input
+						<br />
+						<TextField
+							id="zipcode-input"
+							label="Zip Code"
+							variant="outlined"
+							size="small"
+							inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+							onChange={(e) => handleChange(e)}
+							onKeyDown={(e) => handleEnterKey(e)}
+							value={zipCode}
 							error={isInputError}
-							onKeyDown={handleEnterKey}
-							onChange={(event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-								setZipCode(event.target.value);
-							}}
-						></Input>
-					</FormControl>
+							helperText={helperText}
+						/>
+					</FormGroup>
 					{/* <AppBar>Location Data Needed</AppBar> */}
 				</Paper>
 			</Container>
